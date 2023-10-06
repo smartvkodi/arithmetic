41ee75470b044b677547c4e69c61a906b76442f0
Ciprian Popa <smartv.kodi@gmail.com>
2023-10-06 19:56:31 +0300

    initial comnit

---
+++ arithmetic.go
@@ -0,0 +1,14 @@
+// Exercise 1. Create a package called arithmetic
+// in $GOPATH/packages/mypackages
+// and paste the following code in the file:
+package arithmetic
+
+// Checks if a number is prime or not
+func IsPrime(num int) bool {
+	for i := 2; i < int(num/2); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smartvkodi/arithmetic
+
+go 1.21.2
